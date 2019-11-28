5cdec1a98f58857899e5194ef2e39f19720a8b51
Toby Merz <realtiaz@gmail.com>
2019-11-28 11:42:07 +0100

    add required packages

--- linbox.sh
+++ linbox.sh
@@ -425,15 +425,18 @@ phase_gentoo() {
 		--numeric-owner \
 		-C "${MOUNTPOINT}"
 
-    mountpseudofs "${MOUNTPOINT}"
+	mountpseudofs "${MOUNTPOINT}"
 
 	info "Syncing the Gentoo repository"
 	cat <<-_EOL | chroot "${MOUNTPOINT}" /bin/sh
+		source /etc/profile
 		emerge-webrsync
 	_EOL
 
 	info "Setting up users"
 	cat <<-_EOL | chroot "${MOUNTPOINT}" /bin/sh
+		source /etc/profile
+
 		cat <<-_EOP | passwd
 			${LINBOX_ROOT_PASSWORD}
 			${LINBOX_ROOT_PASSWORD}
@@ -450,9 +453,30 @@ phase_gentoo() {
 		_EOP
 	_EOL
 
-    umountsubvols "${MOUNTPOINT}"
-    unmountpseudofs "${MOUNTPOINT}"
-    closecrypt
+	info "Installing required packages"
+	cat <<-_EOL | chroot "${MOUNTPOINT}" /bin/sh
+		echo "sys-apps/systemd cryptsetup" >> /etc/portage/package.use
+		echo "sys-boot/grub device-mapper" >> /etc/portage/package.use
+		echo "sys-fs/cryptsetup static kernel -gcrypt" >> /etc/portage/package.use
+		echo "sys-kernel/genkernel-next cryptsetup" >> /etc/portage/package.use
+		echo "sys-kernel/dracut systemd device-mapper" >> /etc/portage/package.use
+
+		echo "sys-fs/btrfs-progs ~amd64" >> /etc/portage/package.keywords
+        echo "sys-boot/grub:2 ~amd64" >> /etc/portage/package.keywords
+        echo "sys-fs/cryptsetup ~amd64" >> /etc/portage/package.keywords
+        echo "sys-kernel/genkernel-next ~amd64" >> /etc/portage/package.keywords
+        echo "sys-kernel/gentoo-sources ~amd64" >> /etc/portage/package.keywords
+
+        emerge --ask systemd grub:2 cryptsetup genkernel-next btrfs-progs dracut gentoo-sources
+	_EOL
+
+	umountsubvols "${MOUNTPOINT}"
+	unmountpseudofs "${MOUNTPOINT}"
+	closecrypt
+}
+
+phase_gento_postinstall() {
+    return
 }
 
 main() {
