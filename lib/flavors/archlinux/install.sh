135286204670e8e8a8111c82f2e87134d6d0cc6b
shizonic <realtiaz@gmail.com>
2019-12-15 12:39:46 +0000

    reoder location of pre- and install functions for archlinux

--- lib/flavors/archlinux/install.sh
+++ lib/flavors/archlinux/install.sh
@@ -4,31 +4,6 @@ opencrypt
 mountrootfs "archlinux" "${MOUNTPOINT}"
 mountpseudofs "${MOUNTPOINT}"
 
-info "Copying archlinux rootfs"
-{
-	copychroot "${TMPDIR}/root.x86_64" "/"
-} >/dev/null 2>&1
-
-info "Removing unused files from rootfs"
-{
-	cmdchroot "rm -f /README"
-} >/dev/null 2>&1
-
-info "Copying crypt.key file"
-{
-	copychroot "${TMPDIR}/crypt" "/boot"
-} >/dev/null 2>&1
-
-info "Backing up pacman mirrorlist"
-{
-	cmdchroot "cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp"
-} >/dev/null 2>&1
-
-info "Copying pacman mirror list to rootfs"
-{
-	copychroot "lib/flavors/archlinux/files/etc/pacman.d" "/etc/pacman.d"
-} >/dev/null 2>&1
-
 info "Initialize pacman keyring"
 {
 	cmdchroot "pacman-key --init && pacman-key --populate"
--- lib/flavors/archlinux/preinstall.sh
+++ lib/flavors/archlinux/preinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+opencrypt
+mountrootfs "archlinux" "${MOUNTPOINT}"
+
 info "Retreiving archlinux bootstrap tarball"
 {
 	wget --directory "${TMPDIR}" \
@@ -13,3 +16,31 @@ info "Extracting archlinux bootstrap tarball"
 		--numeric-owner \
 		-C "${TMPDIR}"
 } >/dev/null 2>&1
+
+info "Copying archlinux rootfs"
+{
+	copychroot "${TMPDIR}/root.x86_64" "/"
+} >/dev/null 2>&1
+
+info "Removing unused files from rootfs"
+{
+	cmdchroot "rm -f /README"
+} >/dev/null 2>&1
+
+info "Copying crypt.key file"
+{
+	copychroot "${TMPDIR}/crypt" "/boot"
+} >/dev/null 2>&1
+
+info "Backing up pacman mirrorlist"
+{
+	cmdchroot "cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp"
+} >/dev/null 2>&1
+
+info "Copying pacman mirror list to rootfs"
+{
+	copychroot "lib/flavors/archlinux/files/etc/pacman.d" "/etc/pacman.d"
+} >/dev/null 2>&1
+
+unmountrootfs "${MOUNTPOINT}"
+closecrypt
